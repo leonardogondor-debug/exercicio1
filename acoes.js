c50605b4f3329beb461a6b7afa8560c82753efc5
leovit <leonardogondor@gmail.com>
2025-09-14 00:21:34 -0300

    corrige a lógica de classificação do IMC e adiciona evento de clique no botão de cálculo

--- acoes.js
+++ acoes.js
@@ -6,6 +6,12 @@ function pesoaltura() {
     let resultado = imc.toFixed(2);
 
     if (imc < 18.5) {
-        
+        document.getElementById(classificao).textContent ="voce esta abaixo do peso";
+    }
+    else if (imc < 24.9) {
+        document.getElementById(classificacao).textContent ="voce esta com o peso normal";
+    }
+    else if (imc < 29.9) {
+        document.getElementById(classificacao).textContent ="voce esta com sobrepeso";
     }
 }
